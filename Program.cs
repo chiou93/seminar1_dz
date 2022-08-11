5207c2b8555d704b3fc13b98d5334a3be9b574b5
Leisan <dororoqw@gmail.com>
2022-08-12 00:20:05 +0300

    задача 8 решение

--- Program.cs
+++ Program.cs
@@ -1,68 +1,26 @@
-﻿/* Задача 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿/* Задача 8 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3 */
-
-/*
-int a = 5;
-int b = 7;
-int max = a;
-int min = a;
-if (b>a)
-{
-    max = b;
-}
-if (a<b)
-{
-    min = a;
-}
-Console.WriteLine("Наибольшее число " + max);
-Console.WriteLine("Наименьшее число "  + min); */
-
-/*
-int a = 2;
-int b = 10;
-int max = a;
-int min = a;
-if (b>a)
-{
-    max = b;
-}
-if (a<b)
-{
-    min = a;
-}
-Console.WriteLine("Наибольшее число " + max);
-Console.WriteLine("Наименьшее число "  + min); */
-
-/*
-int a = -9;
-int b = -3;
-int max = a;
-int min = a;
-if (b>a)
-{
-    max = b;
-}
-if (a<b)
-{
-    min = a;
-}
-Console.WriteLine("Наибольшее число " + max);
-Console.WriteLine("Наименьшее число "  + min); */
+5 -> 2, 4
+8 -> 2, 4, 6, 8
 
 
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int.Parse(Console.ReadLine()!); 
 
-/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Write number 1: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Write number 2: "); */
 
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22*/
-
-
-
-
-
-            
-
+        Console.Write("Введите начальное число: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите конечное число: ");
+        int m = int.Parse(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " " + " ");
+            }
+        }
+        Console.ReadKey();

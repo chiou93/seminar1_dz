4c56bc64908524a1a8082b71fa777c495b59c49a
Leisan <dororoqw@gmail.com>
2022-08-11 14:48:46 +0300

    Задача 2 решение

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+/*
+int a = 5;
+int b = 7;
+int max = a;
+int min = a;
+if (b>a)
+{
+    max = b;
+}
+if (a<b)
+{
+    min = a;
+}
+Console.WriteLine("Наибольшее число " + max);
+Console.WriteLine("Наименьшее число "  + min); */
+
+/*
+int a = 2;
+int b = 10;
+int max = a;
+int min = a;
+if (b>a)
+{
+    max = b;
+}
+if (a<b)
+{
+    min = a;
+}
+Console.WriteLine("Наибольшее число " + max);
+Console.WriteLine("Наименьшее число "  + min); */
+
+/*
+int a = -9;
+int b = -3;
+int max = a;
+int min = a;
+if (b>a)
+{
+    max = b;
+}
+if (a<b)
+{
+    min = a;
+}
+Console.WriteLine("Наибольшее число " + max);
+Console.WriteLine("Наименьшее число "  + min); */
+
+
+
+/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+
+
+
+
+            
+
